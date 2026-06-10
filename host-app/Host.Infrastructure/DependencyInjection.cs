706939d89513018b163d5a3daef63af4f029a7e2
PhamTrungDuc <ptrungduc1011@gmail.com>
2026-06-11 00:57:21 +0700

    update

--- host-app/Host.Infrastructure/DependencyInjection.cs
+++ host-app/Host.Infrastructure/DependencyInjection.cs
@@ -55,7 +55,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        var connectionString = configuration["SQLString"];
 
         services.AddDbContext<HostContext>(options =>
             options.UseSqlServer(connectionString));
